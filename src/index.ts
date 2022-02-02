8ae8deb51f93afce83f23417ea3c22edd7a2e368
Layo Folaranmi <thedarkkid.codes@gmail.com>
2022-02-02 02:03:39 +0100

    experience tests and todos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-export function multiply(a:number, b:number) {
-  return a * b;
-}
+import { Class } from './lib/Class';
+import Writer from './lib/Writer';
+
+// const class_ = new Class('Meta');
+// class_.write((content: Writer) => {
+//   content.add('// class comment');
+// });
+
+// const fn = Class.create('Meta', (content) => {
+//   content.add('// class comment');
+// })
+// console.log(fn.toString());
+
+// Todo: write tests;
+// Todo: figure out a way to do class Exports;
+// Todo: Differentiate between a class/File
