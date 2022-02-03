abceec9f89917d99ca934735b75a78cc01faf1a3
Layo Folaranmi <thedarkkid.codes@gmail.com>
2022-02-03 11:29:24 +0100

    allowed entities to have multiple arguements in the constructor

--- src/lib/Entity.ts
+++ src/lib/Entity.ts
@@ -4,14 +4,14 @@ export default abstract class Entity {
   protected name: string;
   protected content: Writer;
 
-  protected constructor (name: string) {
+  protected constructor (name: string, ...args:any[]) {
     this.name = name;
     this.content = new Writer();
   }
 
   abstract write (write: WriterFunction);
 
-  static _create<T extends Entity> (eConstructor: new (name: string) => T, name: string, writerFn: WriterFunction): T {
+  static _create<T extends Entity> (eConstructor: new (name: string, ...args:any[]) => T, name: string, writerFn: WriterFunction): T {
     const _ent = new (eConstructor)(name);
     _ent.write(writerFn);
     return _ent;
